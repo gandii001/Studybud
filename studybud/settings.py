f15ba4907cebded9e45be9613787474ef6318d40
gandii001 <surajudeen2003@gmail.com>
2026-01-02 01:14:05 +0000

    code commit

--- studybud/settings.py
+++ studybud/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['studybud.com','www.studybud.com', '*.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app','studybud.com','www.studybud.com', '*.onrender.com', '127.0.0.1']
 
 
 # Application definition
